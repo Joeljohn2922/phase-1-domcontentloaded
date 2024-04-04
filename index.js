b5b1ca36250dd9d20442345d3344107aaf6cce46
Joel John <johnjoel079@gmail.com>
2024-04-04 15:01:25 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Your code goes here 
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  
+  var paragraph = document.getElementById("text") 
+  paragraph.textContent = "This is really cool!"; 
+
+}); 
